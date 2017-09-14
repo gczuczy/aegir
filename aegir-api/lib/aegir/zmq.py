fab67cd6da5f90ada3c411bb5ccf26f4428f34c1
Gergely Czuczy <gergely.czuczy@harmless.hu>
2017-09-14 17:49:34 +0000

    [api] Reconnect to zmq if the connection fails

--- aegir-api/lib/aegir/zmq.py
+++ aegir-api/lib/aegir/zmq.py
@@ -4,6 +4,7 @@ provides a highlevel communication API for the app
 '''
 
 import zmq
+from pprint import pprint
 
 import aegir.config
 
@@ -18,11 +19,21 @@ def instance_init():
     global _ctx, _socket_pr
 
     _ctx = zmq.Context()
+    reconnect_socket();
+    pass
+
+def reconnect_socket():
+    global _ctx, _socket_pr
+
+    if not _socket_pr == None:
+        _socket_pr.close()
+        _socket_pr = None
+        pass
+
     _socket_pr = _ctx.socket(zmq.REQ)
     _socket_pr.connect('tcp://127.0.0.1:{port}'.format(port = aegir.config.config['prport']))
     _socket_pr.setsockopt(zmq.RCVTIMEO, 1000)
     _socket_pr.setsockopt(zmq.LINGER, 0)
-    pass
 
 def prmessage(command, data):
     global _socket_pr
@@ -31,6 +42,8 @@ def prmessage(command, data):
            'data': data}
     try:
         _socket_pr.send_json(msg)
+        return _socket_pr.recv_json()
     except Exception:
+        reconnect_socket()
         raise Exception("brewd is not running")
-    return _socket_pr.recv_json()
+    pass
