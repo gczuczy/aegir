54efa7820c272ea4dca37216acb51fa3052e3c28
Gergely Czuczy <gergely.czuczy@harmless.hu>
2018-02-12 06:01:25 +0000

    [brewd] ElapsedTime only reports above 70ms now

--- aegir-brewd/src/ElapsedTime.cc
+++ aegir-brewd/src/ElapsedTime.cc
@@ -6,13 +6,15 @@
 namespace aegir {
 
   ElapsedTime::ElapsedTime(std::string _message): c_message(_message) {
-    printf("ElapsedTime start: %s\n", c_message.c_str());
+    //printf("ElapsedTime start: %s\n", c_message.c_str());
     c_start = std::chrono::high_resolution_clock::now();
   }
 
   ElapsedTime::~ElapsedTime() {
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end-c_start;
-    printf("ElapsedTime end: %s %.4f ms\n", c_message.c_str(), diff.count()*1000);
+    float cnt = diff.count()*1000;
+    if ( cnt > 70 )
+      printf("ElapsedTime: %s %.4f ms\n", c_message.c_str(), cnt);
   }
 }
