22b99998f951d05a3c1436df0164ac2fccfde412
Gergely Czuczy <gergely.czuczy@harmless.hu>
2018-03-09 16:54:41 +0000

    Replaced the ui-switch module with ngx-ui-switch.
    The angular2-ui-switch doesn't support AOT. I wonder what is it good for actually...

--- aegir-ui/src/app/maintenance/maintenance.module.ts
+++ aegir-ui/src/app/maintenance/maintenance.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { UiSwitchModule } from 'angular2-ui-switch';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 import { MaintenanceComponent } from './maintenance.component';
 
