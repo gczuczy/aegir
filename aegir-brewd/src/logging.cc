d72c9c5c905b3d6c44d953b27c5b13a160374861
Gergely Czuczy <gergely.czuczy@harmless.hu>
2023-11-17 14:50:29 +0000

    initial syslog sink works

--- aegir-brewd/src/logging.cc
+++ aegir-brewd/src/logging.cc
@@ -4,19 +4,35 @@
 
 #include "logging.hh"
 
+#define BOOST_LOG_USE_NATIVE_SYSLOG
+
 #include <boost/log/core.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/attributes/attribute.hpp>
 #include <boost/log/attributes/current_thread_id.hpp>
+#include <boost/log/sinks/syslog_backend.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+
+#ifdef BOOST_LOG_WITHOUT_SYSLOG
+#error "BOOST_LOG_WITHOUT_SYSLOG is set"
+#endif
+
+#ifndef BOOST_LOG_USE_NATIVE_SYSLOG
+#error "BOOST_LOG_USE_NATIVE_SYSLOG is unset"
+#endif
 
 namespace bl = ::boost::log;
 namespace blt = ::boost::log::trivial;
 namespace bla = ::boost::log::attributes;
+namespace bls = ::boost::log::sinks;
+namespace blk = ::boost::log::keywords;
 //namespace blf = boost::log::filters;
 
 namespace aegir {
   namespace logging {
+
+    typedef bls::synchronous_sink<bls::syslog_backend> syslog_sink_t;
 
     static bool filter(const boost::log::attribute_value_set&);
 
@@ -28,6 +44,18 @@ namespace aegir {
 				   bla::current_thread_id()
 				   );
       blcore->set_filter(&filter);
+
+      // init syslog
+      {
+	boost::shared_ptr<bls::syslog_backend> backend(new bls::syslog_backend(
+									       blk::facility = bls::syslog::daemon,
+									       blk::use_impl = bls::syslog::native,
+									       blk::ident = "aegir"
+));
+	backend->set_severity_mapper(bls::syslog::direct_severity_mapping<int>("Severity"));
+
+	blcore->add_sink(boost::make_shared<syslog_sink_t>(backend));
+      }
     }
 
     void setSeverity(blt::severity_level _level) {
