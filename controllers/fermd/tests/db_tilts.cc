62021c34e138581fe0d82a52231ec4d53bd9a92c
Gergely Czuczy <gergely.czuczy@harmless.hu>
2024-12-26 15:31:50 +0000

    High adjust works

--- controllers/fermd/tests/db_tilts.cc
+++ controllers/fermd/tests/db_tilts.cc
@@ -15,39 +15,33 @@ using Catch::Matchers::WithinRel;
 using Catch::Matchers::WithinAbs;
 using Catch::Matchers::WithinULP;
 
-TEST_CASE_METHOD(DBFixture, "db_tilts", "[fermd][db][tilts]") {
-  auto txn = db()->txn();
-  auto tilts = txn->getTilthydrometers();
-  REQUIRE(tilts.size() == 8);
-
-  auto& cth = *tilts.begin();
-  aegir::fermd::DB::tilthydrometer th = *cth;
-  th.enabled = true;
-  txn.setTilthydrometer(th);
-
-  // now verify
-  auto th2 = txn->getTilthydrometerByUUID(th.uuid);
-  REQUIRE(th2->enabled == th.enabled);
-}
-
-static float randrange(float _range) {
-	return _range*(1.0f*arc4random())/static_cast<float>(std::numeric_limits<uint32_t>::max());
-
-}
-
-TEST_CASE("TiltAdjustNone", "[fermd][db][tilts][adjust]") {
+TEST_CASE("TiltAdjustNone", "[fermd][tilts][adjust]") {
 	aegir::fermd::DB::tilthydrometer th;
 	float sg = GENERATE(take(5, random(1.000f, 1.120f)));
 	REQUIRE_THAT(th.adjust(sg), Catch::Matchers::WithinAbs(sg, 0.0001));
 }
 
-TEST_CASE("TiltAdjustNull", "[fermd][db][tilts][adjust]") {
+TEST_CASE("TiltAdjustNull", "[fermd][tilts][adjust]") {
 	aegir::fermd::DB::tilthydrometer th;
 	float offset = GENERATE(take(5, random(-0.005f, 0.010f)));
 	float measured = GENERATE(take(5, random(1.000f, 1.120f)));
 	float expected = measured - offset;
 
 	th.setZero(1.000f+offset);
+	float adjusted = th.adjust(measured);
+
+	REQUIRE_THAT(adjusted,
+							 WithinAbs(expected, 0.0001f)
+							 || WithinRel(expected, 0.0005f) );
+}
+
+TEST_CASE("TiltAdjustHigh", "[fermd][tilts][adjust]") {
+	aegir::fermd::DB::tilthydrometer th;
+	float offset = GENERATE(take(5, random(-0.005f, 0.010f)));
+	float measured = GENERATE(take(5, random(1.000f, 1.120f)));
+	float expected = measured - offset;
+
+	th.setHigh(expected, measured);
 	float adjusted = th.adjust(measured);
 
 	REQUIRE_THAT(adjusted,
